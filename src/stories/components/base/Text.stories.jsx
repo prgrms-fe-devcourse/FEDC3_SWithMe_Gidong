21358f2abb33f77bc29887abb95e6ff9810352a7
hyejun <solar3070@naver.com>
2023-01-11 01:23:03 +0900

    feat: base 컴포넌트 추가 (헤더, 이미지, 텍스트)

---
+++ src/stories/components/base/Text.stories.jsx
@@ -0,0 +1,45 @@
+import Text from '@/components/base/Text';
+
+export default {
+  title: 'Component/Text',
+  component: Text,
+  argTypes: {
+    size: { control: 'number' },
+    strong: { control: 'boolean' },
+    underline: { control: 'boolean' },
+    delete: { control: 'boolean' },
+    color: { control: 'color' },
+    block: { control: 'boolean' },
+    paragraph: { control: 'boolean' },
+    mark: { control: 'boolean' },
+    code: { control: 'boolean' },
+  },
+};
+
+export const Default = (args) => {
+  return (
+    <>
+      <Text {...args}>Text</Text>
+      <Text {...args}>Text</Text>
+    </>
+  );
+};
+
+export const Size = (args) => {
+  return (
+    <>
+      <Text {...args} size='large'>
+        Large
+      </Text>
+      <Text {...args} size='normal'>
+        Normal
+      </Text>
+      <Text {...args} size='small'>
+        Small
+      </Text>
+      <Text {...args} size={24}>
+        Custom
+      </Text>
+    </>
+  );
+};
