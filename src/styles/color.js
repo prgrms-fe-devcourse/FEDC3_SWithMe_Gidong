8981b981f7c28bdad4682ebfcd6579dcdea77d9c
hyejun <solar3070@naver.com>
2023-01-11 00:31:48 +0900

    chore: 컬러 파일 추가

---
+++ src/styles/color.js
@@ -0,0 +1,8 @@
+export const COLOR = {
+  WHITE: '#fff',
+  DARK: '#3A3A3A',
+  MY_GROUP_BG: '#F4F5FF',
+  MY_GROUP_BTN_BG: '#EAECFC',
+  MY_GROUP_BTN_FONT: '#738DFA',
+  MY_GROUP_BOX_BG: '#F7E9EC',
+};
