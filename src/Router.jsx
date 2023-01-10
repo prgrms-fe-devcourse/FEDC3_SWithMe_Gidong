999a36d035a1914eed75d8e9f09ed161dedc211e
hyejun <solar3070@naver.com>
2023-01-11 01:23:42 +0900

    feat: 내 그룹 페이지 라우트 설정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import Home from '../pages/Home';
+import MyGroup from './pages/MyGroup';
 
 function Router() {
   return (
     <BrowserRouter>
-      <Routes>{/* <Route path='/' element={<Home />} /> */}</Routes>
+      <Routes>
+        {/* <Route path='/' element={<Home />} /> */}
+        <Route path='/myGroup' element={<MyGroup />} />
+      </Routes>
     </BrowserRouter>
   );
 }
