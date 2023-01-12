0bf14db76474cbec73022029584f30b544f5882b
hyejun <solar3070@naver.com>
2023-01-13 01:05:19 +0900

    feat: TIL 리스트 보여주기

--- src/components/domain/TILList/index.jsx
+++ src/components/domain/TILList/index.jsx
@@ -1,11 +1,13 @@
 import styled from '@emotion/styled';
 import { useTILContext } from '@/context/TILProvider';
 import { useState, useCallback, useEffect } from 'react';
+import TILItem from '@/components/domain/TILItem';
+import { Spinner } from '@/components/base';
+import { COLOR } from '@/styles/color';
 
 function TILList({ groupId }) {
   const [isLoading, setIsLoading] = useState(false);
   const { tils, onShowTILByGroup } = useTILContext();
-  console.log('tils', tils);
 
   const handleShowTILsByGroup = useCallback(
     async (groupId) => {
@@ -21,18 +23,37 @@ function TILList({ groupId }) {
   }, []);
 
   return (
-    <StyledTILList>
-      {tils.map((til) => {
-        return <li key={til.id}>{til.title.title}</li>;
-      })}
-    </StyledTILList>
+    <>
+      {isLoading ? (
+        <StyledSpinnerWrapper>
+          <Spinner size={40} color={COLOR.TAG_COLOR[1]} />
+        </StyledSpinnerWrapper>
+      ) : (
+        <StyledTILList>
+          {tils?.map((til) => (
+            <TILItem key={til.title} til={til} />
+          ))}
+        </StyledTILList>
+      )}
+    </>
   );
 }
 
 export default TILList;
 
+const StyledSpinnerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 3rem;
+`;
+
 const StyledTILList = styled.div`
-  display: block;
-  height: 10rem;
-  margin: 1rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(24rem, 1fr));
+  gap: 3rem 0;
+  padding: 1rem;
+
+  justify-items: center;
+  align-items: center;
 `;
