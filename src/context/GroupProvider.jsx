e72a809d35f5d754e35739f42b1947c70b454b64
hyejun <solar3070@naver.com>
2023-01-12 14:50:00 +0900

    feat: 그룹 프로바이더 생성

---
+++ src/context/GroupProvider.jsx
@@ -0,0 +1,42 @@
+import { useReducer, useEffect, createContext, useContext } from 'react';
+
+const GroupContext = createContext();
+export const useGroupContext = () => useContext(GroupContext);
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INIT_GROUPS': {
+      return action.payload;
+    }
+    case 'ADD_GROUP': {
+      // pass
+      break;
+    }
+    case 'UPDATE_GROUP': {
+      // pass
+      break;
+    }
+    case 'DELETE_GROUP': {
+      // pass
+      break;
+    }
+    default: {
+      console.error('Wrong type');
+      break;
+    }
+  }
+};
+
+const GroupProvider = ({ children, initialGroups }) => {
+  const [groups, dispatch] = useReducer(reducer, initialGroups);
+
+  console.log(groups);
+
+  useEffect(() => {
+    dispatch({ type: 'INIT_GROUPS', payload: initialGroups || [] });
+  }, [initialGroups]);
+
+  return <GroupContext.Provider value={{ groups }}>{children}</GroupContext.Provider>;
+};
+
+export default GroupProvider;
--- src/pages/MyGroup.jsx
+++ src/pages/MyGroup.jsx
@@ -1,65 +1,41 @@
 import styled from '@emotion/styled';
 import { COLOR } from '@/styles/color';
 import { imgPuzzle } from '@/assets/images';
-import Image from '@/components/base/Image';
-import Header from '@/components/base/Header';
-import Text from '@/components/base/Text';
-
-const tempGroupData = [
-  {
-    _id: 1,
-    name: '개발하는 돌맹이',
-    desc: '어쩌구저쩌구 공부하는 그룹입니다. 열심히 공부하실 분들만 들어오세요!',
-  },
-  {
-    _id: 2,
-    name: '개발하는 돌맹이',
-    desc: '어쩌구저쩌구 공부하는 그룹입니다. 열심히 공부하실 분들만 들어오세요!',
-  },
-  {
-    _id: 3,
-    name: '개발하는 돌맹이',
-    desc: '어쩌구저쩌구 공부하는 그룹입니다. 열심히 공부하실 분들만 들어오세요!',
-  },
-  {
-    _id: 4,
-    name: '개발하는 돌맹이',
-    desc: '어쩌구저쩌구 공부하는 그룹입니다. 열심히 공부하실 분들만 들어오세요!',
-  },
-];
+import { Image, Header, Text } from '@/components';
+import { getChannelList } from '../api/channel';
+import { useAsync } from '@/hooks';
+import GroupList from '@/components/domain/GroupList';
+import GroupProvider from '@/context/GroupProvider';
 
 function MyGroup() {
+  const initialGroups = useAsync(getChannelList, []);
+
   return (
-    <StyledPageWrapper>
-      <StyledMyGroup>
-        <StyledHeader>
-          <Header size={30}>내 그룹</Header>
-          <button>
-            <i className='fa-solid fa-circle-plus'></i>
-            그룹 만들기
-          </button>
-        </StyledHeader>
-        <StyledDesc>
-          <div>
-            <Text paragraph color={COLOR.DARK} size={21}>
-              내가 <Text>가입한 그룹의 목록</Text>입니다.
-            </Text>
-            <Text paragraph color={COLOR.DARK} size={21}>
-              그룹 내의 멤버들과 소통하며 <Text>오늘의 TIL</Text>을 작성해보세요.
-            </Text>
-          </div>
-          <Image src={imgPuzzle} width={300} />
-        </StyledDesc>
-        <StyledGroupList>
-          {tempGroupData.map((group) => (
-            <StyledGroup key={group._id}>
-              <p>{group.name}</p>
-              <p>{group.desc}</p>
-            </StyledGroup>
-          ))}
-        </StyledGroupList>
-      </StyledMyGroup>
-    </StyledPageWrapper>
+    <GroupProvider initialGroups={initialGroups.value}>
+      <StyledPageWrapper>
+        <StyledMyGroup>
+          <StyledHeader>
+            <Header size={30}>내 그룹</Header>
+            <button>
+              <i className='fa-solid fa-circle-plus'></i>
+              그룹 만들기
+            </button>
+          </StyledHeader>
+          <StyledDesc>
+            <div>
+              <Text paragraph color={COLOR.DARK} size={21}>
+                내가 <Text>가입한 그룹의 목록</Text>입니다.
+              </Text>
+              <Text paragraph color={COLOR.DARK} size={21}>
+                그룹 내의 멤버들과 소통하며 <Text>오늘의 TIL</Text>을 작성해보세요.
+              </Text>
+            </div>
+            <Image src={imgPuzzle} width={300} />
+          </StyledDesc>
+          <GroupList />
+        </StyledMyGroup>
+      </StyledPageWrapper>
+    </GroupProvider>
   );
 }
 
@@ -125,13 +101,4 @@ const StyledDesc = styled.div`
       }
     }
   }
-`;
-
-const StyledGroupList = styled.div`
-  border-radius: 1rem;
-  background-color: white;
-`;
-
-const StyledGroup = styled.div`
-  padding: 10rem;
 `;
