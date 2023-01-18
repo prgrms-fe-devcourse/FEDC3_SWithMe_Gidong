bc7b586147a055f7dfe35f066702c400c4d9c486
hanyugeon <hanyugeon@naver.com>
2023-01-18 22:43:02 +0900

    chore: 안쓰는 아이콘 정리

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -1,8 +1,4 @@
 // export { default as react } from './react.svg'; 이런식으로 작성
-export { default as icAlertOff } from './ic_alert_off.svg';
-export { default as icAlertOn } from './ic_alert_on.svg';
-export { default as icMyGroup } from './ic_my_group.svg';
 export { default as icSearchSubmit } from './ic_search_submit.svg';
-export { default as icMyInfo } from './ic_my_info.svg';
 export { default as icCrown } from './ic_crown.svg';
 export { default as icLogo } from './ic_logo.svg';
