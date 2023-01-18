b72fa5c2eb8c58282e779838f10bacdd9a783926
hanyugeon <hanyugeon@naver.com>
2023-01-18 23:20:54 +0900

    design: 검색 필터 드롭다운 스타일링

---
+++ src/components/domain/Header/HeaderIcon.jsx
@@ -0,0 +1,17 @@
+import { Button, Icon } from '@/components/base';
+import { COLOR } from '@/styles/color';
+
+const HeaderIcon = (props) => {
+  const { onClick, icon } = props;
+
+  return (
+    <Button
+      style={{ width: '3.5rem', height: '3.5rem', marginRight: '1rem', padding: '0' }}
+      bgcolor={COLOR.HEADER_TRANSPARENT_BG}
+      onClick={onClick}>
+      <Icon name={icon} />
+    </Button>
+  );
+};
+
+export default HeaderIcon;
