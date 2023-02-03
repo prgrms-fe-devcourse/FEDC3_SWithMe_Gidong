40d6354d12382ef46221fc9d9113a917c4ea5806
류건열 <rjsduf0503@naver.com>
2023-02-03 20:22:06 +0900

    feat: 알린 보낸 사람의 아바타 표시 및 스타일 수정

--- src/components/domain/AlarmModal/index.jsx
+++ src/components/domain/AlarmModal/index.jsx
@@ -1,7 +1,9 @@
 import { getAlarms } from '@/api/alarm';
-import { Header, Text } from '@/components/base';
+import { imgDefaultAvatar } from '@/assets/images';
+import { Avatar, Header, Text } from '@/components/base';
 import useClickAway from '@/hooks/useClickAway';
 import { COLOR } from '@/styles/color';
+import { convertDate } from '@/utils/date';
 import styled from '@emotion/styled';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -19,7 +21,7 @@ function AlarmModal({ visible, onClose }) {
 
     (async () => {
       const alarms = await getAlarms();
-      alarms?.sort((a, b) => new Date(a.updatedAt) - new Date(b.updatedAt));
+      alarms?.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
 
       setAlarms(alarms);
     })();
@@ -40,11 +42,17 @@ function AlarmModal({ visible, onClose }) {
           {alarms && alarms.length !== 0 ? (
             alarms.map((alarm) => (
               <StyledAlarm key={alarm._id} onClick={() => handleAlarmClick(alarm.post)}>
-                <Text size={1.6} color={'black'}>
-                  {alarm.like
-                    ? `${alarm.author.fullName}님이 ${alarm.like.post.title.title}에 댓글을 남겼습니다.`
-                    : `${alarm.author.fullName}님이 ${alarm.comment.post.title.title}에 공감을 남겼습니다.`}
-                </Text>
+                <StyledAvatar src={alarm.author.image || imgDefaultAvatar} size={4} />
+                <StyledContentContainer>
+                  <StyledContent size={1.6} color={'black'}>
+                    {alarm.like
+                      ? `${alarm.author.fullName}님이 ${alarm.like.post.title.title}에 공감을 남겼습니다.`
+                      : `${alarm.author.fullName}님이 ${alarm.comment.post.title.title}에 남긴 댓글: ${alarm.comment.comment}`}
+                  </StyledContent>
+                  <Text size={1.2} color={COLOR.CREATEDAT}>
+                    {convertDate(new Date(alarm.like ? alarm.like.post.updatedAt : alarm.comment.post.updatedAt))}
+                  </Text>
+                </StyledContentContainer>
               </StyledAlarm>
             ))
           ) : (
@@ -68,9 +76,10 @@ const StyledModalContainer = styled.div`
   top: 4rem;
   right: 12rem;
 
-  width: 24rem;
-  height: 30rem;
-  padding-bottom: 2rem;
+  width: 48rem;
+  height: 64rem;
+  max-width: 48rem;
+  max-height: 64rem;
 
   background-color: ${COLOR.WHITE};
   border-radius: 1rem;
@@ -79,26 +88,60 @@ const StyledModalContainer = styled.div`
 
   display: flex;
   flex-direction: column;
+
+  @media (max-width: 624px) {
+    position: fixed;
+    top: 0;
+    right: 0;
+
+    max-width: 100%;
+    width: 100%;
+  }
 `;
 
 const StyledHeader = styled(Header)`
-  padding: 2rem 2rem 1rem 2rem;
+  display: flex;
+  align-items: center;
+
+  min-height: 4.8rem;
+  padding-left: 2rem;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 `;
 
 const StyledAlarmContainer = styled.div`
   display: flex;
   flex-direction: column;
-  height: calc(100% - 4rem);
   overflow: auto;
 `;
 
 const StyledAlarm = styled.div`
   cursor: pointer;
-  margin: 0 1rem;
-  padding: 0.6rem 1rem;
-  border-radius: 0.5rem;
+  display: flex;
+  padding: 1.6rem 1.6rem 1.6rem 0;
 
   &:hover {
-    background-color: ${COLOR.GRAY};
+    background-color: ${COLOR.TEXTAREA_BG};
   }
+`;
+
+const StyledContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-right: 1rem;
+`;
+
+const StyledContent = styled(Text)`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-height: 6rem;
+  -webkit-line-clamp: 3;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  white-space: normal;
+`;
+
+const StyledAvatar = styled(Avatar)`
+  align-self: flex-start;
+  margin: 0 1.6rem;
 `;
