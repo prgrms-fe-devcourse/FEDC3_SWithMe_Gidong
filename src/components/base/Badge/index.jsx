68835e7011e63b60e3d9b3454f1485b21bc80199
hanyugeon <hanyugeon@naver.com>
2023-03-23 01:50:26 +0900

    refact: Badge 컴포넌트 스타일 코드 분리

--- src/components/base/Badge/index.jsx
+++ src/components/base/Badge/index.jsx
@@ -1,5 +1,4 @@
-import { COLOR } from '@/styles/color';
-import styled from '@emotion/styled';
+import { StyledBadgeContainer, StyledSuper } from './styles';
 
 function Badge({ children, count, maxCount, showZero, dot = false, bgColor, textColor, ...props }) {
   const colorStyle = {
@@ -27,33 +26,3 @@ function Badge({ children, count, maxCount, showZero, dot = false, bgColor, text
 }
 
 export default Badge;
-
-const StyledBadgeContainer = styled.div`
-  position: relative;
-  display: inline-block;
-`;
-
-const StyledSuper = styled.sup`
-  position: absolute;
-  top: 0;
-  right: 0;
-  display: inline-flex;
-  align-items: center;
-  height: 2rem;
-  padding: 0 0.8rem;
-  font-size: 1.2rem;
-  border-radius: 2rem;
-  color: white;
-  background-color: ${COLOR.RED};
-  transform: translate(50%, -50%);
-
-  &.dot {
-    padding: 0;
-    width: 1rem;
-    height: 1rem;
-    border-radius: 50%;
-
-    top: 0.2rem;
-    right: 0.3rem;
-  }
-`;
