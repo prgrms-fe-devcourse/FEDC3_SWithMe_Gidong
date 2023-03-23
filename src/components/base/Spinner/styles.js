f7fc04fd97449baee1e4e0a8c4ebe5cb318288c0
hanyugeon <hanyugeon@naver.com>
2023-03-23 16:22:36 +0900

    refact: Spinner 컴포넌트 스타일 코드 분리

---
+++ src/components/base/Spinner/styles.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const StyledSpinnerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 3rem;
+`;
+
+const Icon = styled.i`
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+export { StyledSpinnerWrapper, Icon };
