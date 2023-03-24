4d806bb0439bc4466663222844b367e4992ebbe0
hanyugeon <hanyugeon@naver.com>
2023-03-24 14:25:54 +0900

    refactor: SearchResult 스타일 코드 분리

--- src/components/domain/SearchResult/SearchResultGroup.jsx
+++ src/components/domain/SearchResult/SearchResultGroup.jsx
@@ -1,7 +1,7 @@
 import { Button, Header, Icon, Text } from '@/components/base';
 import { COLOR } from '@/styles/color';
-import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
+import { StyledGroupUserAmount, StyledResultGroupCard, StyledResultGroupTags } from './styles';
 
 function SearchResultGroup({ group, index }) {
   const navigate = useNavigate();
@@ -38,46 +38,3 @@ function SearchResultGroup({ group, index }) {
 }
 
 export default SearchResultGroup;
-
-const StyledResultGroupCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 0.8rem;
-
-  width: 22rem;
-  height: 30rem;
-  padding: 2rem;
-  border-radius: 2rem;
-  background-color: ${COLOR.WHITE};
-
-  h2 {
-    width: 18rem;
-    text-align: center;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-  }
-`;
-
-const StyledGroupUserAmount = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  gap: 0.5rem;
-  width: 100%;
-`;
-
-const StyledResultGroupTags = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-content: center;
-  gap: 1rem;
-
-  width: 100%;
-  height: 17.6rem;
-  padding: 1.8rem 0;
-  border-radius: 2.4rem;
-  background-color: ${COLOR.GRAY_10};
-  color: ${COLOR.GRAY4};
-`;
---
+++ src/components/domain/SearchResult/styles.js
@@ -0,0 +1,86 @@
+import { COLOR } from '@/styles/color';
+import styled from '@emotion/styled';
+
+const StyledSearchResultContainer = styled.div`
+  position: relative;
+  flex: 1;
+  padding: 10rem 10rem 0 10rem;
+`;
+
+const StyledHeader = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 1rem;
+`;
+
+const StyledSearchResult = styled.div`
+  margin: 3rem 0;
+  padding: 3rem;
+  border-radius: 1rem;
+  background-color: ${COLOR.GRAY_10};
+`;
+
+const StyledGroupList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 24rem);
+  gap: 3rem 0;
+  padding: 1rem;
+
+  justify-content: center;
+  justify-items: center;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+const StyledResultGroupCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.8rem;
+
+  width: 22rem;
+  height: 30rem;
+  padding: 2rem;
+  border-radius: 2rem;
+  background-color: ${COLOR.WHITE};
+
+  h2 {
+    width: 18rem;
+    text-align: center;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+`;
+
+const StyledGroupUserAmount = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 0.5rem;
+  width: 100%;
+`;
+
+const StyledResultGroupTags = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  gap: 1rem;
+
+  width: 100%;
+  height: 17.6rem;
+  padding: 1.8rem 0;
+  border-radius: 2.4rem;
+  background-color: ${COLOR.GRAY_10};
+  color: ${COLOR.GRAY4};
+`;
+
+export {
+  StyledSearchResultContainer,
+  StyledHeader,
+  StyledSearchResult,
+  StyledGroupList,
+  StyledResultGroupCard,
+  StyledGroupUserAmount,
+  StyledResultGroupTags,
+};
