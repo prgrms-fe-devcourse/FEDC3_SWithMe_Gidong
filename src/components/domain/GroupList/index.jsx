a6250cf0d55c904ea102e8166eaba4283ac7a801
hyejun <solar3070@naver.com>
2023-03-29 12:07:48 +0900

    refactor: 그룹 목록 get 쿼리 적용

--- src/components/domain/GroupList/index.jsx
+++ src/components/domain/GroupList/index.jsx
@@ -3,45 +3,58 @@ import { Empty, Spinner } from '@/components/base';
 import GroupItem from '@/components/domain/GroupItem';
 import TILList from '@/components/domain/TILList';
 import { useAuthContext } from '@/context/AuthProvider';
-import { useGroupContext } from '@/context/GroupProvider';
 import { COLOR } from '@/styles/color';
 import { Fragment, useEffect, useState } from 'react';
+import { useGetGroupList } from '@/hooks/queries/group';
 import { StyledGroupList } from './styles';
+import { setItem, getItem } from '@/utils/sessionStorage';
 
 function GroupList() {
-  const {
-    groups: { value: groups, isLoading },
-    openedGroupId,
-  } = useGroupContext();
+  const groupList = useGetGroupList();
+  const [openedGroupId, setOpenedGroupId] = useState(getItem('openedGroupId'));
   const {
     authState: { loggedUser },
   } = useAuthContext();
   const [myGroupList, setMyGroupList] = useState([]);
 
   useEffect(() => {
+    setItem('openedGroupId', openedGroupId);
+  }, [openedGroupId]);
+
+  useEffect(() => {
     setMyGroupList(
-      groups?.filter(
+      groupList.data?.filter(
         ({ description }) =>
           description.master === loggedUser._id || description.member.some((el) => el === loggedUser._id),
       ),
     );
-  }, [groups]);
+  }, [groupList.data]);
 
   return (
     <>
-      {isLoading ? (
+      {groupList.isLoading ? (
         <Spinner size={40} color={COLOR.TAG_COLOR[1]} />
       ) : (
         <StyledGroupList isEmpty={!myGroupList?.length}>
           {myGroupList?.length ? (
             myGroupList.map((group, i) => (
               <Fragment key={group._id}>
-                <GroupItem group={group} isLastGroup={myGroupList.length - 1 === i} />
+                <GroupItem
+                  group={group}
+                  isLastGroup={myGroupList.length - 1 === i}
+                  openedGroupId={openedGroupId}
+                  setOpenedGroupId={setOpenedGroupId}
+                />
                 {openedGroupId === group._id && <TILList groupId={group._id} groupName={group.name} />}
               </Fragment>
             ))
           ) : (
-            <Empty src={imgSearch} imageWidth='25rem' mainText='참여중인 그룹이 없습니다.' subText='그룹에 참여해보세요!' />
+            <Empty
+              src={imgSearch}
+              imageWidth='25rem'
+              mainText='참여중인 그룹이 없습니다.'
+              subText='그룹에 참여해보세요!'
+            />
           )}
         </StyledGroupList>
       )}
