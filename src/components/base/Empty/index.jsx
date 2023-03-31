91924421b0ddef992e84b99778cf5c7c3ec94216
hanyugeon <hanyugeon@naver.com>
2023-03-31 11:36:58 +0900

    chore: Empty 컴포넌트 불필요한 props 제거

--- src/components/base/Empty/index.jsx
+++ src/components/base/Empty/index.jsx
@@ -2,7 +2,7 @@ import { Image, Text } from '@/components/base';
 import PropTypes from 'prop-types';
 import * as S from './styles';
 
-function Empty({ src, imageWidth, imageHeight, mainText, subText, ...props }) {
+function Empty({ src, imageWidth, imageHeight, mainText, subText }) {
   return (
     <S.Empty>
       <Image src={src} width={imageWidth} height={imageHeight} alt={subText} />
